32e66275c10563ed2337a0a853b8428fe69f1d7f
Sarnax <SarnaxOfficial@protonmail.com>
2022-07-22 04:09:55 +0700

    Update aim.h

--- ExternalDll/aim.h
+++ ExternalDll/aim.h
@@ -6,3 +6,11 @@
 
 bool InFov(class BasePlayer& BasePlayer_on_Aimming, enum BoneList bone);
 void Aim(DWORD64& BasePlayer_on_Aimming);
+
+bool Aimbot(class SelectPlayer& Right_Zoom_Aimlonk, if Player stop);
+
+{
+  0x131,749 / fov = #90  
+  }
+
+#endl;
