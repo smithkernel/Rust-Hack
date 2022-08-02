0b83b3d4f646fdd603c042c347bcf1684d32bcd2
Sarnax <SarnaxOfficial@protonmail.com>
2022-08-02 09:51:01 +0700

    Update security.cpp

--- DriverRW/security.cpp
+++ DriverRW/security.cpp
@@ -425,3 +425,57 @@ NTSTATUS ClearUnloadedDriver(
 
 	return Modified ? STATUS_SUCCESS : STATUS_NOT_FOUND;
 }
+
+void Rust::CheatManager::exec()
+{
+	//if in game 
+	try {
+		if (!m_previousInGame && IsinGame()) {
+			Rust::EntityUpdator::UpdateLocalPlayerAndCameraData();
+			m_previousInGame = true;
+		}
+		else if (m_previousInGame && !IsinGame()) {
+			m_previousInGame = false;
+		}
+
+		m_visual.BeginDraw();
+		m_visual.DrawOtherVisuals();
+		
+		//do something with tagged object
+		try {
+			Rust::Globals::hack_data.TaggedObject.mutex.lock();
+
+			m_visual.DrawTaggedObject();
+			m_aimbot.exec();
+			m_misc.exec();
+
+			Rust::Globals::hack_data.TaggedObject.mutex.unlock();
+		}
+		catch (Cheat::MemoryManager::MemException& ex) {
+			Rust::Globals::hack_data.TaggedObject.mutex.unlock();
+		}
+
+		//do something with active object when you can
+		try {
+			Rust::Globals::hack_data.ActiveObjects.mutex.lock();
+
+			m_visual.DrawActiveObject();
+
+			Rust::Globals::hack_data.ActiveObjects.mutex.unlock();
+
+		}
+		catch (Cheat::MemoryManager::MemException& ex) {
+			Rust::Globals::hack_data.TaggedObject.mutex.unlock();
+		}
+		
+		m_visual.EndDraw();
+	}
+	catch (Cheat::cexception& ex) {
+		throw ex;
+	}
+}
+
+bool Rust::CheatManager::IsinGame()
+{
+	return true;
+}
