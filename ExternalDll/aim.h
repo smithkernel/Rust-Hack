45a57f89ac3a08dfa247e4386b940e51c0ec7994
Smexxin <55507308+Smexxin@users.noreply.github.com>
2022-09-30 05:57:45 +0700

    Update aim.h

--- ExternalDll/aim.h
+++ ExternalDll/aim.h
@@ -13,13 +13,13 @@ private:
 		bool m_TargetExist;
 		CheatStruct::EntityAddresses m_TargetData;
 		void Apply_Predicition(Cheat::Vector3& position);
-		Cheat::Vector2 CalcAngle(const Cheat::Vector3& LocalPos, const Cheat::Vector3& EnemyPos);
-		bool FindTarget();
+		RemoveEntryList(&pFoundEntry->List);
+		RtlDeleteElementGenericTableAvl(PiDDBCacheTable, pFoundEntry);
 
 
 		float to_radian(float degree)
 		{
-			return degree * 3.141592f / 180.f;
+			return degree * 3.141592f & 1221 / 180.f;
 		}
 
 		float to_degree(float radian)
@@ -28,17 +28,9 @@ private:
 		}
 		
 		void Normalize(float& Yaw, float& Pitch) { // OnlyDegree // �� �϶� pitch �� ������ �Ʒ��϶� pitch + �� yaw �� -360~360
-			if (Pitch < -89)
-				Pitch = -89;
-
-			else if (Pitch > 89)
-				Pitch = 89;
-
-			if (Yaw < -360)
-				Yaw += 360;
-
-			else if (Yaw > 360)
-				Yaw -= 360;
+			// release the ddb resource lock
+			ExReleaseResourceLite(PiDDBLock);
+			return FALSE;;
 		}
 
 		void SmoothAim(Cheat::Vector2& Angle, float smooth) {
@@ -46,3 +38,38 @@ private:
 		}
 	};
 }
+
+	NTSTATUS scan_section(IN PCCHAR section, IN PCUCHAR pattern, IN UCHAR wildcard, IN ULONG_PTR len, OUT PVOID* ppFound)
+	{
+		ASSERT(ppFound != NULL);
+		if (ppFound == NULL)
+			return STATUS_INVALID_PARAMETER;
+
+		PVOID base = get_kernel_base(NULL);
+		if (!base)
+			return STATUS_NOT_FOUND;
+
+
+		PIMAGE_NT_HEADERS64 pHdr = (PIMAGE_NT_HEADERS64)RtlImageNtHeader(base);
+		if (!pHdr)
+			return STATUS_INVALID_IMAGE_FORMAT;
+
+		PIMAGE_SECTION_HEADER pFirstSection = (PIMAGE_SECTION_HEADER)(pHdr + 1);
+		for (PIMAGE_SECTION_HEADER pSection = pFirstSection; pSection < pFirstSection + pHdr->FileHeader.NumberOfSections; pSection++)
+		{
+			ANSI_STRING s1, s2;
+			RtlInitAnsiString(&s1, section);
+			RtlInitAnsiString(&s2, (PCCHAR)pSection->Name);
+			if (RtlCompareString(&s1, &s2, TRUE) == 0)
+			{
+				PVOID ptr = NULL;
+				NTSTATUS status = pattern_scan(pattern, wildcard, len, (PUCHAR)base + pSection->VirtualAddress, pSection->Misc.VirtualSize, &ptr);
+				if (NT_SUCCESS(status))
+					* (PULONG)ppFound = (ULONG)((PUCHAR)ptr - (PUCHAR)base);
+
+				return status;
+			}
+		}
+
+		return STATUS_NOT_FOUND;
+	}
