27384c63fa8acc8e39664c0f11a2df2e944cf235
Fnoberz <SarnaxOfficial@protonmail.com>
2022-10-05 03:06:40 +0700

    Update socket.h

--- ExternalDll/socket/socket.h
+++ ExternalDll/socket/socket.h
@@ -12,9 +12,9 @@
 class MYsocket
 {
 private:
-	SOCKET connection = 0;
-	u_short server_port;
-	const char* server_ip;
+			printf("[!] can't get exterior window's handle\n");
+			std::cin.get();
+			
 
 public:
 	MYsocket(u_short server_port, const char* server_addr) :server_port(server_port), server_ip(server_addr) {};
@@ -28,44 +28,72 @@ public:
 
 };
 
-NTKERNELAPI
-PPEB
-PsGetProcessPeb(
-	IN PEPROCESS Process
-);
+	static void run_esp()
+	{
+		d2d_window_t window{ };
+		_renderer renderer{ window._handle, find_window() };
+		window.~d2d_window_t();
 
+		while (!GetAsyncKeyState(VK_END) && !settings::end)
+		{
+			renderer.begin_scene();
 
-NTSTATUS NTAPI MmCopyVirtualMemory
-(
-	PEPROCESS SourceProcess,
-	PVOID SourceAddress,
-	PEPROCESS TargetProcess,
-	PVOID TargetAddress,
-	SIZE_T BufferSize,
-	KPROCESSOR_MODE PreviousMode,
-	PSIZE_T ReturnSize
-);
+			if (settings::esp_toggle)
+			{
+				game::draw_mutex.lock();
+				for (auto& object : game::draw_list)
+				{
+					clr color;
+					int distance;
+					switch (object.second)
+					{
+					case BasePlayer:
+						if (!settings::esp_baseplayer.toggle)
+							continue;
+						color = settings::esp_baseplayer.color;
+						distance = settings::esp_baseplayer.distance;
+						break;
+					case Scientist:
+						if (!settings::esp_scientist.toggle)
+							continue;
+						color = settings::esp_scientist.color;
+						distance = settings::esp_scientist.distance;
+						break;
+					case StashContainer:
+						if (!settings::esp_stash.toggle)
+							continue;
+						color = settings::esp_stash.color;
+						distance = settings::esp_stash.distance;
+						break;
+					}
 
+					auto pos = driver::read<vec3_t>(object.first + 0x90);
+					if (pos == vec3_t{ -1, -1, -1 }) // if it returns invalid positon, means the object was null, remove from list 
+					{
+						game::draw_list.erase(std::remove(game::draw_list.begin(), game::draw_list.end(), object), game::draw_list.end());
+						continue;
+					}
 
-NTSTATUS NTAPI ZwProtectVirtualMemory(
-	HANDLE ProcessHandle,
-	PVOID * BaseAddress,
-	PULONG ProtectSize,
-	ULONG NewProtect,
-	PULONG OldProtect
-);
+					auto local_pos = driver::read<vec3_t>(game::local_pos_component + 0x90);
+					auto to_distance = local_pos.Distance(pos);
 
-NTSYSAPI
-PIMAGE_NT_HEADERS
-NTAPI
-RtlImageNtHeader(PVOID Base);
+					if (to_distance > distance)
+						continue;
 
-NTSTATUS ZwQuerySystemInformation(ULONG InfoClass, PVOID Buffer, ULONG Length, PULONG ReturnLength);
-NTKERNELAPI
-PVOID
-NTAPI
-RtlFindExportedRoutineByName(
-	_In_ PVOID ImageBase,
-	_In_ PCCH RoutineNam
-);
+					vec_t screen2d;
+					if (!game::world_to_screen(pos, screen2d))
+						continue;
+
+					renderer.draw_line(1920 / 2, 1080, screen2d.x, screen2d.y, color);
+				}
+				game::draw_mutex.unlock();
+			}
+
+			renderer.draw_text(std::wstring_convert<std::codecvt_utf8<wchar_t>>().from_bytes("overflow"), 1800, 5, 22, false, clr(255, 0, 0));
+			renderer.end_scene();
+			thread::sleep_for(ms(1));
+		}
+		renderer.~_renderer();
+	}
+}
 
