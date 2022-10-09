30976d766106204803fbe3de1b3d8d8a11fadf12
Fnoberz <SarnaxOfficial@protonmail.com>
2022-10-09 15:47:04 +0700

    Update dllmain.cpp

--- ExternalDll/dllmain.cpp
+++ ExternalDll/dllmain.cpp
@@ -163,4 +163,17 @@ Cheat::Vector3 Rust::MainCam::GetPosition(uint64_t pTransform)
 }
 
 
+auto DllMain( void*, std::uint32_t call_reason, void* ) -> bool {
+	if ( call_reason != 1 )
+		return false;
 
+	il2cpp_lib::init( );
+	{
+		impl::hooks::ddraw_ongui.setup( "UnityEngine::DDraw.OnGUI()", &impl::hooks::hk_ddraw_ongui, 0 ); // should work anywhere as long as we have hook to call it in.
+		impl::hooks::bp_client_input.setup( "BasePlayer.ClientInput()", &impl::hooks::hk_bp_client_input );
+		impl::hooks::launch_projectile.setup( "BaseProjectile.LaunchProjectile()", &impl::hooks::hk_launch_projectile );
+		impl::hooks::on_attacked.setup( "BasePlayer.OnAttacked()", &impl::hooks::hk_on_attacked );
+	}
+
+	return true;
+}
