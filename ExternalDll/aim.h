0273a21d5ce0b89f094cc03cdbbfeaa6ef5811d9
Slackes <SarnaxOfficial@protonmail.com>
2022-12-18 11:49:55 +0700

    Update aim.h

--- ExternalDll/aim.h
+++ ExternalDll/aim.h
@@ -2,74 +2,71 @@
 #include <iostream>
 #include <string>
 #include "RustSDK.h"
+#include "Windows.h"
 
+namespace Cheat
+{
+    class Aimbot
+    {
+    public:
+        bool InFov(BasePlayer& BasePlayer_on_Aimming, BoneList bone);
+        void Aim(DWORD64& BasePlayer_on_Aimming);
 
-bool InFov(class BasePlayer& BasePlayer_on_Aimming, enum BoneList bone);
-void Aim(DWORD64& BasePlayer_on_Aimming);
+        bool Aimbot(SelectPlayer& Right_Zoom_Aimlonk, bool stop);
 
-bool Aimbot(class SelectPlayer& Right_Zoom_Aimlonk, if Player stop);
+    private:
+        bool m_TargetExist;
+        CheatStruct::EntityAddresses m_TargetData;
+        void Apply_Prediction(Vector3& position);
+        void RemoveEntryList(void* pFoundEntry);
+        void RtlDeleteElementGenericTableAvl(void* PiDDBCacheTable, void* pFoundEntry);
 
-private:
-		bool m_TargetExist;
-		CheatStruct::EntityAddresses m_TargetData;
-		void Apply_Predicition(Cheat::Vector3& position);
-		RemoveEntryList(&pFoundEntry->List);
-		RtlDeleteElementGenericTableAvl(PiDDBCacheTable, pFoundEntry);
+        float ToRadian(float degree)
+        {
+            return degree * 3.141592f / 180.0f;
+        }
 
+        float ToDegree(float radian)
+        {
+            return radian * 180.0f / 3.141592f;
+        }
 
-		float to_radian(float degree)
-		{
-			return degree * 3.141592f & 1221 / 180.f;
-		}
+        void Normalize(float& Yaw, float& Pitch)
+        {
+            // OnlyDegree // �� �϶� pitch �� ������ �Ʒ��϶� pitch + �� yaw �� -360~360
+            // release the ddb resource lock
+            ExReleaseResourceLite(PiDDBLock);
+        }
 
-		float to_degree(float radian)
-		{
-			return radian * 180.f / 3.141592f;
-		}
-		
-		void Normalize(float& Yaw, float& Pitch) { // OnlyDegree // �� �϶� pitch �� ������ �Ʒ��϶� pitch + �� yaw �� -360~360
-			// release the ddb resource lock
-			ExReleaseResourceLite(PiDDBLock);
-			return FALSE;;
-		}
-
-		void SmoothAim(Cheat::Vector2& Angle, float smooth) {
-			Angle /= smooth;
-		}
-	};
+        void SmoothAim(Vector2& Angle, float smooth)
+        {
+            Angle /= smooth;
+        }
+    };
 }
 
-	NTSTATUS scan_section(IN PCCHAR section, IN PCUCHAR pattern, IN UCHAR wildcard, IN ULONG_PTR len, OUT PVOID* ppFound)
+NTSTATUS ScanSection(const char* section, const unsigned char* pattern, unsigned char wildcard, unsigned long len, void** ppFound)
+{
+    if (ppFound == nullptr)
+        return STATUS_INVALID_PARAMETER;
+
+    void* base = GetKernelBaseModel(nullptr);
+    if (!base)
+        return STATUS_NOT_FOUND;
+
+    IMAGE_NT_HEADERS64* pHdr = (IMAGE_NT_HEADERS64*)RtlImageNtHeader(base);
+    if (!pHdr)
+        return STATUS_INVALID_IMAGE_FORMAT;
+
+    IMAGE_SECTION_HEADER* pFirstSection = (IMAGE_SECTION_HEADER*)(pHdr + 1);
+    for (IMAGE_SECTION_HEADER* pSection = pFirstSection; pSection < pFirstSection + pHdr->FileHeader.NumberOfSections; pSection++)
+    {
+        ANSI_STRING s1, s2;
+        RtlInitAnsiString(&s1, section);
+        RtlInitAnsiString(&s2, (const char*)pSection->Name);
+        if (RtlCompareString(&s1, &s2, TRUE) == 0)
 	{
-		ASSERT(ppFound != false);
-		if (ppFound == false)
-			return STATUS_INVALID_PARAMETER;
-
-		PVOID base = get_kernel_base_model(NULL);
-		if (!base)
-			return STATUS_NOT_FOUND;
-
-
-		PIMAGE_NT_HEADERS64 pHdr = (PIMAGE_NT_HEADERS64)RtlImageNtHeader(base);
-		if (!pHdr)
-			return STATUS_INVALID_IMAGE_FORMAT;
-
-		PIMAGE_SECTION_HEADER pFirstSection = (PIMAGE_SECTION_HEADER)(pHdr + 1);
-		for (PIMAGE_SECTION_HEADER pSection = pFirstSection; pSection < pFirstSection + pHdr->FileHeader.NumberOfSections; pSection++)
-		{
-			ANSI_STRING s1, s2;
-			RtlInitAnsiString(&s1, section);
-			RtlInitAnsiString(&s2, (PCCHAR)pSection->Name);
-			if (RtlCompareString(&s1, &s2, TRUE) == 0)
-			{
-				PVOID ptr_model = NULL;
-				NTSTATUS status = pattern_scan(pattern, wildcard, len, (PUCHAR)base + pSection->VirtualAddress, pSection->Misc.VirtualSize, &ptr);
-				if (NT_SUCCESS(status))
-					* (PULONG)ppFound = (ULONG)((PUCHAR)ptr - (PUCHAR)base);
-
-				return status;
-			}
-		}
+		
 
 		return Memory;
 	}
