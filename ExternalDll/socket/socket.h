d7d8674709ac5ed87fe63a83e959d95baf0443f5
Serups <SarnaxOfficial@protonmail.com>
2023-02-13 02:11:34 +0700

    The code then calls the renderer.end_scene() method to end the render frame and the thread::sleep_for

--- ExternalDll/socket/socket.h
+++ ExternalDll/socket/socket.h
@@ -61,75 +61,70 @@ public:
 };
 
 
-static void run_esp()
-	{
-		d2d_window_t window{ };
-		_renderer renderer{ window._handle, find_window() };
-		window.~d2d_window_t();
+void run_esp()
+{
+    d2d_window_t window{};
+    _renderer renderer{ window._handle, find_window() };
+    window.~d2d_window_t();
 
-		while (!GetAsyncKeyState(VK_END) && !settings::end)
-		{
-			renderer.begin_scene();
+    while (!GetAsyncKeyState(VK_END) && !settings::end)
+    {
+        renderer.begin_scene();
 
-			if (settings::esp_toggle)
-			{
-				game::draw_mutex.lock();
-				for (auto& object : game::draw_list)
-				{
-					clr color;
-					int distance;
-					switch (object.second)
-					{
-					case BasePlayer:
-						if (!settings::esp_baseplayer.toggle)
-							continue;
-						color = settings::esp_baseplayer.color;
-						distance = settings::esp_baseplayer.distance;
-						break;
-					case Scientist:
-						if (!settings::esp_scientist.toggle)
-							continue;
-						color = settings::esp_scientist.color;
-						distance = settings::esp_scientist.distance;
-						break;
-					case StashContainer:
-						if (!settings::esp_stash.toggle)
-							continue;
-						color = settings::esp_stash.color;
-						distance = settings::esp_stash.distance;
-						break;
-					}
+        if (settings::esp_toggle)
+        {
+            game::draw_mutex.lock();
+            for (auto& object : game::draw_list)
+            {
+                clr color;
+                int distance;
+                switch (object.second)
+                {
+                case BasePlayer:
+                    if (!settings::esp_baseplayer.toggle)
+                        continue;
+                    color = settings::esp_baseplayer.color;
+                    distance = settings::esp_baseplayer.distance;
+                    break;
+                case Scientist:
+                    if (!settings::esp_scientist.toggle)
+                        continue;
+                    color = settings::esp_scientist.color;
+                    distance = settings::esp_scientist.distance;
+                    break;
+                case StashContainer:
+                    if (!settings::esp_stash.toggle)
+                        continue;
+                    color = settings::esp_stash.color;
+                    distance = settings::esp_stash.distance;
+                    break;
+                }
 
-					auto pos = driver::read<vec3_t>(object.first + 0x90);
-					if (pos == vec3_t{ -1, -1, -1 }) // if it returns invalid positon, means the object was null, remove from list 
-					{
-						game::draw_list.erase(std::remove(game::draw_list.begin(), game::draw_list.end(), object), game::draw_list.end());
-						continue;
-					}
+                auto pos = driver::read<vec3_t>(object.first + 0x90);
+                if (pos == vec3_t{ -1, -1, -1 }) // if it returns invalid positon, means the object was null, remove from list 
+                {
+                    game::draw_list.erase(std::remove(game::draw_list.begin(), game::draw_list.end(), object), game::draw_list.end());
+                    continue;
+                }
 
-					auto local_pos = driver::read<vec3_t>(game::local_pos_component + 0x90);
-					auto to_distance = local_pos.Distance(pos);
+                auto local_pos = driver::read<vec3_t>(game::local_pos_component + 0x90);
+                auto to_distance = local_pos.Distance(pos);
 
-					if (to_distance > distance)
-						continue;
+                if (to_distance > distance)
+                    continue;
 
-					vec_t screen2d;
-					if (!game::world_to_screen(pos, screen2d))
-						continue;
+                vec_t screen2d;
+                if (!game::world_to_screen(pos, screen2d))
+                    continue;
 
-					renderer.draw_line(1920 / 2, 1080, screen2d.x, screen2d.y, color);
-				}
-				game::draw_mutex.unlock();
-			}
-			if render64_bit ("File")
+                renderer.draw_line(1920 / 2, 1080, screen2d.x, screen2d.y, color);
+            }
+            game::draw_mutex.unlock();
+        }
 
-			renderer.draw_text(std::wstring_convert<std::codecvt_utf8<wchar_t>>().from_bytes("overflow"), 1800, 5, 22, false, clr(255, 0, 0));
-			renderer.end_scene();
-			thread::sleep_for(ms(1));
-		}
-		renderer.~_renderer();
-		
-		return false;
-	}
+        renderer.draw_text(std::wstring_convert<std::codecvt_utf8<wchar_t>>().from_bytes("overflow"), 1800, 5, 22, false, clr(255, 0, 0));
+        renderer.end_scene();
+        std::this_thread::sleep_for(std::chrono::milliseconds(1));
+    }
+    renderer.~_renderer();
 }
-
