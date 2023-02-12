35dd2206b0262f5a4eb3a57d2af0d6873ecd633a
Serups <SarnaxOfficial@protonmail.com>
2023-02-13 01:11:16 +0700

    In this corrected version, I have added descriptions for each field in the structures to provide more context.

--- DriverRW/ioctls.h
+++ DriverRW/ioctls.h
@@ -10,30 +10,30 @@ typedef unsigned long long uint64_t;
 
 
 typedef struct _k_alloc_mem_request {
-	ULONG pid;
-	ULONG allocation_type;
-	ULONG protect;
-	ULONGLONG addr;
-	SIZE_T size;
+  ULONG pid;           // The process ID of the process making the request
+  ULONG allocation_type; // The type of memory allocation
+  ULONG protect;        // The protection type for the allocated memory
+  ULONGLONG addr;       // The starting address of the allocated memory
+  SIZE_T size;          // The size of the memory allocation
 } k_alloc_mem_request, *pk_alloc_mem_request;
 
 typedef struct _k_get_base_module_request {
-	ULONG pid;
-	ULONGLONG handle;
-	WCHAR name[260];
-} k_get_base_module_request, * pk_get_base_module_request;
+  ULONG pid;           // The process ID of the process making the request
+  ULONGLONG handle;     // The handle of the base module
+  WCHAR name[260];      // The name of the base module
+} k_get_base_module_request, *pk_get_base_module_request;
 
 typedef struct _k_protect_mem_request {
-	ULONG pid;
-	ULONG protect;
-	ULONGLONG addr;
-	SIZE_T size;
+  ULONG pid;           // The process ID of the process making the request
+  ULONG protect;        // The protection type for the memory
+  ULONGLONG addr;       // The starting address of the memory
+  SIZE_T size;          // The size of the memory
 } k_protect_mem_request, *pk_protect_mem_request;
 
 typedef struct _k_rw_request {
-	uint32_t src_pid;
-	uint32_t dst_pid;
-	uint64_t src_addr;
-	uint64_t dst_addr;
-	SIZE_T size;
+  uint32_t src_pid;     // The process ID of the source process
+  uint32_t dst_pid;     // The process ID of the destination process
+  uint64_t src_addr;    // The starting address of the source memory
+  uint64_t dst_addr;    // The starting address of the destination memory
+  SIZE_T size;          // The size of the memory being read/written
 } k_rw_request, *pk_rw_request;
