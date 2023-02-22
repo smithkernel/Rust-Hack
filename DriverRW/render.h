962fb6d99f12338202519917573c17fbf3448d2c
Rain <118963202+DonrChase@users.noreply.github.com>
2023-02-22 23:29:58 +0700

    Delete Something

--- DriverRW/render.h
+++ DriverRW/render.h
@@ -388,24 +388,3 @@ private:
 };
 
 							
-void ProcessesComponent::ProcessesComponent()
-{
-	LookAndFeel::setDefaultLookAndFeel(&lookAndFeelCustom);
-    addAndMakeVisible(processList);
-    processList.setName("process list");
-
-    addAndMakeVisible(okButton = new TextButton("ok button"));
-    okButton->setButtonText(TRANS("Okay"));
-    okButton->addListener(this);
-
-	hNtdll = (HMODULE)Utils::GetLocalModuleHandle("ntdll.dll");
-	fnQSI = (tNTQSI)Utils::GetProcAddress(hNtdll, "NtQuerySystemInformation");
-
-	FetchProcessList();
-	processList.loadData(false); 
-
-	processList.table.getHeader().addColumn("Process", 1, 140, 18);
-	processList.table.getHeader().addColumn("PID", 2, 50, 18);
-
-	setSize(300, 350);
-}
