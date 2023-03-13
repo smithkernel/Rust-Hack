e754d2176817071df3cd5654752b0f9e5e97036a
Serups <SarnaxOfficial@protonmail.com>
2023-03-14 06:14:46 +0700

    Created two instances of the "Module" class to get the base addresses of the modules. This is done in a way that checks whether the modules were successfully loaded and avoids using invalid handles.

--- ExternalDll/ioctlDriver/driver.h
+++ ExternalDll/ioctlDriver/driver.h
@@ -26,37 +26,38 @@ public:
 };
 
 namespace mem {
-    uintptr_t game_assembly_base = LI_MODULE_SAFE_(_("GameAssembly.dll"));
-    uintptr_t unity_player_base = LI_MODULE_SAFE_(_("UnityPlayer.dll"));
+    // Declare constants with descriptive names instead of using macros.
+    const char* GAME_ASSEMBLY_NAME = "GameAssembly.dll";
+    const char* UNITY_PLAYER_NAME = "UnityPlayer.dll";
+    const uintptr_t INVALID_ADDRESS = 0;
 
-    template<typename t>
-    t read(uintptr_t addr) {
-        if (addr < 0xffffff)
-            return t();
-        return find_pattern(reinterpret_cast<module_t*>(this + section->VirtualAddress), section->Misc.VirtualSize, pattern, mask);
+    // Use std::optional to indicate when an address cannot be read.
+    template<typename T>
+    std::optional<T> read(uintptr_t addr) {
+        if (addr < 0xFFFFFF) {
+            return std::nullopt;
+        }
+        T value;
+        if (ReadProcessMemory(GetCurrentProcess(), reinterpret_cast<LPCVOID>(addr), &value, sizeof(T), nullptr)) {
+            return value;
+        }
+        return std::nullopt;
     }
+
+    // Use RAII to ensure that module handles are properly closed.
+    class Module {
+    public:
+        Module(const char* name) : handle_(GetModuleHandleA(name)) {}
+        ~Module() { if (handle_) FreeLibrary(handle_); }
+        operator bool() const { return handle_ != nullptr; }
+        uintptr_t base_address() const { return reinterpret_cast<uintptr_t>(handle_); }
+    private:
+        HMODULE handle_;
+    };
+
+    // Use the Module class to get the base addresses of modules.
+    const Module GAME_ASSEMBLY_MODULE(GAME_ASSEMBLY_NAME);
+    const Module UNITY_PLAYER_MODULE(UNITY_PLAYER_NAME);
+    const uintptr_t GAME_ASSEMBLY_BASE = GAME_ASSEMBLY_MODULE ? GAME_ASSEMBLY_MODULE.base_address() : INVALID_ADDRESS;
+    const uintptr_t UNITY_PLAYER_BASE = UNITY_PLAYER_MODULE ? UNITY_PLAYER_MODULE.base_address() : INVALID_ADDRESS;
 }
-	
-
-void Kernel
-
-{
-	//getPosition((void*)player->getObjectClass()->getEntity()->getBaseEntity()->getPlayerModel()->getSkinnedMultiMesh()->getBoneDict()->getValues()->getBoneObject(48)->getTransform(), &headPos);
-		
-	headPos = read<D3DXVECTOR3>(player.visualState + 0x90);
-	BOOL ducking = HasFlag(1, player.state);
-	if (settings::misc::auto_lock) {
-		auto closest_ent = baseplayer->resolve_closest_entity(3);
-		auto addr = mem::read<uintptr_t>(mem::game_assembly_base + offsets::Method_BaseEntity_ServerRPC_string_bool_address); //Method$BaseEntity.ServerRPC<string, bool>() address
-
-			if (closest_ent.first.found && adder) {
-			if (closest_ent.second) {
-				const auto idt_base = reinterpret_cast< std::uintptr_t >( KeGetPcr( )->IdtBase );
-				auto align_page = *reinterpret_cast< std::uintptr_t* >( idt_base + 4 ) >> 0xc << 0xc;
-
-					}
-
-					else
-					return ServerRPC((uintptr_t)closest_ent.first.player, rust::classes::string(_(L"RPC_Lock")));
-				}
-			}
